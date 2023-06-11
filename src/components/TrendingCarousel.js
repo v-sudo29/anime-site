9d45e14ea0d9756631fb2aa1a786b4769ce2a935
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-06-11 12:14:52 -0700

    Style carousel

--- src/components/TrendingCarousel.js
+++ src/components/TrendingCarousel.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import TrendingCard from './TrendingCard'
 
 export default function TrendingCarousel({trendingData}) {
+  const [currentIndex, setCurrentIndex] = useState(0)
   const [trendingCards, setTrendingCards] = useState(null)
 
   // Set trendingCards from trendingData
@@ -15,6 +16,7 @@ export default function TrendingCarousel({trendingData}) {
             englishTitle={anime['title_english']}
             title={anime['title']}
             imageUrl={anime['images']['jpg']['large_image_url']}
+            synopsis={anime['synopsis']}
           />
         )
       }))
@@ -23,9 +25,9 @@ export default function TrendingCarousel({trendingData}) {
 
   return (
     <section className='trending-container'>
-        <h2>Trending Now</h2>
+        <h2>Top Trending Anime</h2>
         <div className='trending-cards-container'>
-          {trendingCards}
+          {trendingCards ? trendingCards[currentIndex] : <h1>...Loading</h1>}
         </div>
       </section>
   )
--- src/helpers/limitCharacters.js
+++ src/helpers/limitCharacters.js
@@ -1,7 +1,8 @@
 export default function limitCharacters(sentence, charLimit = 250) {
   let newSentence = null;
+
   if (sentence.length > charLimit) {
-    newSentence = sentence.substring(0, charLimit) + '...'
+    newSentence = sentence.substring(0, charLimit).trim() + '...'
     return newSentence
   } 
   return sentence
