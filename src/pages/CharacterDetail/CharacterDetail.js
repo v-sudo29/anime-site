99ee8c5d9eebacb24270aad02c7f3c77c5cdba46
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-07-06 14:58:45 -0700

    Have loader animation take up entire page

--- src/pages/CharacterDetail/CharacterDetail.js
+++ src/pages/CharacterDetail/CharacterDetail.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import styles from '../../styles/components/CharacterDetail.module.css'
-import LoaderAnimation from '../../components/LoaderAnimation'
+// import LoaderAnimation from '../../components/LoaderAnimation'
 import HeroContent from './HeroContent'
 import Stats from './Stats'
 import Biography from './Biography'
 import VoiceActors from './VoiceActors'
+import LoaderAnimation from '../../components/LoaderAnimation'
 
 export default function CharacterDetail() {
   const params = useParams()
@@ -19,20 +20,19 @@ export default function CharacterDetail() {
     }
   }, [character, params.id])
 
-  console.log(character)
-
   return (
-    <div className={styles.detailPage}>
-      <div className={styles.backgroundImg}></div>
-      {character
-        ? <div className={styles.content}>
-            <HeroContent styles={styles} character={character}/>
-            <Stats styles={styles} character={character}/>
-            <Biography styles={styles} character={character}/>
-            <VoiceActors styles={styles} character={character}/>
-          </div>
-        : <LoaderAnimation/>
-      }
-    </div>
+    <>
+      {character ?
+        <div className={styles.detailPage}>
+          <div className={styles.backgroundImg}></div>
+              <div className={styles.content}>
+                  <HeroContent styles={styles} character={character}/>
+                  <Stats styles={styles} character={character}/>
+                  <Biography styles={styles} character={character}/>
+                  <VoiceActors styles={styles} character={character}/>
+              </div>
+        </div>
+      : <LoaderAnimation/>}
+    </>
   )
 }
