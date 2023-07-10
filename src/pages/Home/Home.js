0fa1edbff554dfb37d84554f56d938bb39bd72e6
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-07-10 12:37:06 -0700

    remove useRef

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import LoaderAnimation from '../../components/LoaderAnimation'
 import LatestNews from './LatestNews'
 import TrendingCarousel from './TrendingCarousel'
