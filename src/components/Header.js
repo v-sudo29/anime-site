9c9b0e2adf2e1e72d71530399eec5ac64511b653
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-09-13 15:19:36 -0700

    Convert header styles to css modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styles from '../styles/components/Header.module.css'
 
 function Header() {
   return (
-    <header>
-      <nav className='nav-links'>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
         <NavLink className={({isActive}) => isActive ? 'active-link' : null} to='/'>Home</NavLink>
         <NavLink className={({isActive}) => isActive ? 'active-link' : null} to='/anime-list'>Anime</NavLink>
         <NavLink className={({isActive}) => isActive ? 'active-link' : null} to='/news'>News</NavLink>
