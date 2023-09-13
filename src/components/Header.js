53efa7dcb139a8ed7f934896df5fb7e4a67b1ad3
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-09-13 15:33:34 -0700

    Fix active link underline

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import styles from '../styles/components/Header.module.css'
 function Header() {
   return (
     <header className={styles.header}>
-      <nav className={styles.nav}>
-        <NavLink className={({isActive}) => isActive ? 'active-link' : null} to='/'>Home</NavLink>
-        <NavLink className={({isActive}) => isActive ? 'active-link' : null} to='/anime-list'>Anime</NavLink>
-        <NavLink className={({isActive}) => isActive ? 'active-link' : null} to='/news'>News</NavLink>
+      <nav className={styles.navigation}>
+        <NavLink className={({isActive}) => isActive ? styles.activeLink : null} to='/'>Home</NavLink>
+        <NavLink className={({isActive}) => isActive ? styles.activeLink : null} to='/anime-list'>Anime</NavLink>
+        <NavLink className={({isActive}) => isActive ? styles.activeLink : null} to='/news'>News</NavLink>
       </nav>
     </header>
   )
