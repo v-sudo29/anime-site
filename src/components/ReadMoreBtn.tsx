1eb1950a7a85ea6eff494b2b1e8277be1e754ab7
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-09-21 12:25:40 -0700

    Animate ReadMoreBtn component

--- src/components/ReadMoreBtn.tsx
+++ src/components/ReadMoreBtn.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import yellowArrowIcon from '../assets/yellow-arrow-icon.png'
+import whiteArrowIcon from '../assets/arrow-icon.png'
 import styles from '../styles/components/ReadMoreBtn.module.css'
 
 export default function ReadMoreBtn({ url } : { url: string }) {
   return (
     <div className={styles.container}>
       <Link to={url} tabIndex={-1} className={styles.link} target="_blank" rel="noopener noreferrer">
-        <span>Read More</span>
-        <img src={yellowArrowIcon} alt="arrow icon" />
+        <span className={styles.readMoreSpan}>Read More</span>
+        <div className={styles.arrowsSlideshow}>
+            <div className={styles.arrowsSlider}>
+              <div className={`${styles.arrowsSlide} ${styles.whiteArrowSlide}`}>
+                <img className={styles.whiteArrow} src={whiteArrowIcon} alt="White arrow icon" />
+              </div>
+              <div className={styles.arrowsSlide}>
+                <img className={styles.yellowArrow} src={yellowArrowIcon} alt="Yellow arrow icon" />
+              </div>
+            </div>
+          </div>
       </Link>
     </div>
   )
