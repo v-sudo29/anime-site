4c3e605a07eece1f93ffa5d6fe3504df4f3ced44
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-10-08 14:30:33 -0700

    Restyle show more characters btn

--- src/pages/AnimeDetail/Characters.tsx
+++ src/pages/AnimeDetail/Characters.tsx
@@ -3,6 +3,8 @@ import styles from '../../styles/anime-detail/Characters.module.css'
 import useFetchCharacters from '../../hooks/useFetchCharacters'
 import CharacterCard from './CharacterCard'
 import { AnimeDetailData } from '../../types/fetchDataTypes/fetchAnimeDetailTypes'
+import CarrotDownIcon from '../../icons/CarrotDownIcon'
+import carrotStyles from '../../styles/icons/CarrotDownIcon.module.css'
 
 interface ICharacters {
   anime: AnimeDetailData
@@ -10,9 +12,26 @@ interface ICharacters {
 }
 
 export default function Characters({ anime, id } : ICharacters) {
+  const [buttonClicked, setButtonClicked] = useState(false)
   const { charactersData } = useFetchCharacters(anime, id)
   const [allCharacters, setAllCharacters] = useState(false)
+  const { carrotActive } = carrotStyles
   let characterCards = null
+
+  const animateCarrotIcon = () => {
+    const carrotIcon = document.querySelector(`.${styles.carrotIconContainer} svg`)
+
+    if (carrotIcon) {
+      !buttonClicked ? carrotIcon.classList.add(`${carrotActive}`) :
+        carrotIcon.classList.remove(`${carrotActive}`)
+    }
+  }
+
+  const handleClick = () => {
+    setAllCharacters(prev => !prev)
+    animateCarrotIcon()
+    setButtonClicked(prev => !prev)
+  }
 
   if (charactersData && !allCharacters) characterCards = charactersData.map((character, index) => 
     (index < 9 && 
@@ -41,13 +60,16 @@ export default function Characters({ anime, id } : ICharacters) {
       : <p className={styles.defaultText}>No characters available.</p>
       }
       {charactersData && 
-      <div className={styles.charactersBtnContainer}>       
-          <button 
-            onClick={() => setAllCharacters(prev => !prev)} 
-            className={styles.charactersBtn}
-          >
-            {allCharacters ? 'See Less Characters' : 'See Full Character List'}
-          </button>
+      <div 
+        onClick={handleClick} 
+        className={styles.charactersBtnContainer}
+      >       
+        <button className={styles.charactersBtn}>
+          {allCharacters ? 'See Less Characters' : 'See Full Character List'}
+        </button>
+        <div className={styles.carrotIconContainer}>
+          <CarrotDownIcon/>
+        </div>
       </div>
       }
     </div>
