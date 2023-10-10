0cf881259b1f2a0914cf246249ac7a325e4f8258
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-10-10 08:56:56 -0700

    Store characters data in localStorage

--- src/hooks/useFetchCharacters.ts
+++ src/hooks/useFetchCharacters.ts
@@ -9,32 +9,78 @@ export default function useFetchCharacters(anime: AnimeDetailData, id: string | 
 
   useEffect(() => {
     if (anime && id) {
-      const controller = new AbortController();
-      const signal = controller.signal;
+      // Check localStorage if characters exist
+      const storedCharacters = localStorage.getItem('characters')
+
+      // Fetch characters data if localStorage data does not exist
+      if (!storedCharacters) {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        
+        setLoading(true)
+        const timer = setTimeout(() => {
+            fetch(`https://api.jikan.moe/v4/anime/${id}/characters`, {signal: signal})
+              .then(response => {
+                if (response.ok) return response.json()
+                throw response
+              })
+              .then((data: CharactersResponse) => {
+                setCharactersData(data.data)
+                localStorage.setItem('characters', JSON.stringify({ id: id, data: data.data }))
+              })
+              .catch((error) => {
+                if (signal.aborted) {
+                  console.log('The user aborted the request', error)
+                } else {
+                  console.error('The request failed', error)
+                  setError(true)
+                }
+              })
+        }, 700)
+        
+        return () => {
+          controller.abort()
+          clearTimeout(timer)
+        }
+      }
+
+      // If stored data exists, check if stored characters id matches props id
+      // If not, then fetch new data
+      if (storedCharacters) {
+        const parsedData = JSON.parse(storedCharacters)
+
+        if (parsedData.id === id) {
+          setCharactersData(parsedData.data)
+        } else {
+          const controller = new AbortController();
+          const signal = controller.signal;
+          
+          setLoading(true)
+          const timer = setTimeout(() => {
+              fetch(`https://api.jikan.moe/v4/anime/${id}/characters`, {signal: signal})
+                .then(response => {
+                  if (response.ok) return response.json()
+                  throw response
+                })
+                .then((data: CharactersResponse) => {
+                  setCharactersData(data.data)
+                  localStorage.setItem('characters', JSON.stringify({ id: id, data: data.data }))
+                })
+                .catch((error) => {
+                  if (signal.aborted) {
+                    console.log('The user aborted the request', error)
+                  } else {
+                    console.error('The request failed', error)
+                    setError(true)
+                  }
+                })
+          }, 700)
       
-      setLoading(true)
-      const timer = setTimeout(() => {
-          fetch(`https://api.jikan.moe/v4/anime/${id}/characters`, {signal: signal})
-            .then(response => {
-              if (response.ok) return response.json()
-              throw response
-            })
-            .then((data: CharactersResponse) => {
-              setCharactersData(data.data)
-            })
-            .catch((error) => {
-              if (signal.aborted) {
-                console.log('The user aborted the request', error)
-              } else {
-                console.error('The request failed', error)
-                setError(true)
-              }
-            })
-      }, 700)
-      
-      return () => {
-        controller.abort()
-        clearTimeout(timer)
+          return () => {
+            controller.abort()
+            clearTimeout(timer)
+          }
+        }
       }
     }
   }, [anime, id])
