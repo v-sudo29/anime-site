0ebdf8d5b109f7b342b16c9bf6af2bcc6713ee09
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-10-12 12:30:07 -0700

    Refactor AnimeListCard, delete png

--- src/components/AnimeListCard.tsx
+++ src/components/AnimeListCard.tsx
@@ -15,27 +15,49 @@ export default function AnimeListCard({ anime, index, id } : IAnimeListCard) {
   const { isMobile } = useMobile()
   return (
     <>
+      {/* MOBILE */}
+      {isMobile && (
+        <Link className={styles.card} to={`/anime/${id}`}>
+          <div className={styles.imgContainer}>
+            <span className={styles.ranking}>{index + 1}</span>
+            <img
+              className={styles.image}
+              src={anime['images']['jpg']['large_image_url']}
+              alt={anime['title_english'] ? filterTitle(anime['title_english']) : filterTitle(anime['title'])}
+            />
+          </div>
+          <h3 className={styles.title}>{anime['title_english'] ? filterTitle(anime['title_english']) : filterTitle(anime['title'])}</h3>
+        </Link>
+      )}
+
+      {/* DESKTOP */}
       {!isMobile && (
         <Link className={styles.card} to={`/anime/${id}`}>
           <span className={styles.ranking}>{index + 1}</span>
           <div className={styles.imgContainer}>
-            <img className={styles.image} src={anime['images']['jpg']['large_image_url']} alt={!anime['title_english'] ? filterTitle(anime['title']) : filterTitle(anime['title_english'])}/>
+            <img
+              className={styles.image}
+              src={anime['images']['jpg']['large_image_url']}
+              alt={anime['title_english'] ? filterTitle(anime['title_english']) : filterTitle(anime['title'])}
+            />
           </div>
           <div className={styles.info}>
             <div>
-              <h3 className={styles.title}>{!anime['title_english'] ? filterTitle(anime['title']) : filterTitle(anime['title_english'])}</h3>
+              <h3 className={styles.title}>{anime['title_english'] ? filterTitle(anime['title_english']) : filterTitle(anime['title'])}</h3>
               <span className={styles.type}>{anime['type'] === 'TV' ? 'TV Show' : anime['type']}</span>
             </div>
             <span className={styles.finishedDate}>
+              {/* Airing Status */}
               {anime['status'] === 'Finished Airing' && 'Finished'}
               {anime['status'] === 'Currently Airing' && 'Airing'}
               {anime['status'] === 'Not yet aired' && 'Not Yet Aired'}
 
-                {anime['aired']['prop']['to']['year'] ? 
-                  <>&nbsp;&#x2022; {anime['aired']['prop']['to']['year']}</> :
-                  anime['aired']['prop']['from']['year'] ? <>&nbsp;&#x2022; {anime['aired']['prop']['from']['year']}</> :
-                  <></>
-                }
+              {/* Year Aired */}
+              {anime['aired']['prop']['to']['year'] ? 
+                <>&nbsp;&#x2022; {anime['aired']['prop']['to']['year']}</> :
+                anime['aired']['prop']['from']['year'] ? <>&nbsp;&#x2022; {anime['aired']['prop']['from']['year']}</> :
+                <></>
+              }
             </span>
           </div>
           <div className={styles.score}>
@@ -44,16 +66,6 @@ export default function AnimeListCard({ anime, index, id } : IAnimeListCard) {
               : anime['score'] : '-'
             }
           </div>
-        </Link>
-      )}
-
-      {isMobile && (
-        <Link className={styles.card} to={`/anime/${id}`}>
-          <div className={styles.imgContainer}>
-            <span className={styles.ranking}>{index + 1}</span>
-            <img className={styles.image} src={anime['images']['jpg']['large_image_url']} alt={!anime['title_english'] ? filterTitle(anime['title']) : filterTitle(anime['title_english'])}/>
-          </div>
-          <h3 className={styles.title}>{!anime['title_english'] ? filterTitle(anime['title']) : filterTitle(anime['title_english'])}</h3>
         </Link>
       )}
     </>   
