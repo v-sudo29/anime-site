210b063a9f2bf98d31c8522ce891d6b996aef777
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-10-12 12:40:00 -0700

    Refactor icons from functions to arrow functions

--- src/icons/CarrotDownIcon.tsx
+++ src/icons/CarrotDownIcon.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import styles from '../styles/icons/CarrotDownIcon.module.css'
 
-
 const CarrotDownIcon = () => {
   return (
     <svg className={styles.icon} width="28" height="22" viewBox="0 0 28 22" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M8 8L14 14L20 8" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
-
   )
 }
 
--- src/icons/SearchIcon.tsx
+++ src/icons/SearchIcon.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from '../styles/icons/SearchIcon.module.css'
 
-export default function SearchIcon() {
+const SearchIcon = () => {
   return (
     <svg className={styles.icon} width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M17.6113 17.1113C21.1826 13.54 21.1826 7.74978 17.6113 4.17848C14.04 0.607174 8.24978 0.607174 4.67848 4.17848C1.10717 7.74978 1.10717 13.54 4.67848 17.1113C8.24978 20.6826 14.04 20.6826 17.6113 17.1113ZM17.6113 17.1113L23 22.5" stroke="white" strokeWidth="3" strokeLinejoin="bevel"/>
     </svg>
   )
 }
+
+export default SearchIcon
